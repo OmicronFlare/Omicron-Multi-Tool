6adf75a555d968e45ba997066d8ff41cc01831aa
Omicron <efosaigbinidu8@gmail.com>
2025-03-23 07:39:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 from aero.updates.omicron import *
 from aero.threespam import *
+from aero.tokenshiton import *
 
 
 def nigga():
     while True:
-        titulo("Omicron Multi Tool | omicron.aero")
+        titulo("Omicron Multi Tool | aeromicron")
         clear()
-        print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter(second_shit2)))
+        print(Colorate.Vertical(Colors.green_to_red, Center.XCenter(second_shit2)))
         print(Colorate.Diagonal(Colors.red_to_green, Center.XCenter(window)))
 
-        puta = input(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("[@omicron.aero] > ")))
+        puta = input(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("\n [@omicron.aero] > ")))
 
         if puta == "1" or puta == "01":
+            titulo("Omicron Multi Tool | aeromicron | Webhook Manipulator")
             clear()
-            print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter(second_shit2)))
+            print(Colorate.Vertical(Colors.green_to_red, Center.XCenter(second_shit2)))
             main()
             input(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("Press [ENTER] > ")))
         
         if puta == "2" or puta == "02":
+            titulo("Omicron Multi Tool | aeromicron | Token Disruptor")
             clear()
-            print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter(second_shit2)))
-            print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("Gonna be updating ts soon")))
+            print(Colorate.Vertical(Colors.green_to_red, Center.XCenter(second_shit2)))
+            run()
             input(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("Press [ENTER] > ")))
         
         if puta == "03" or puta == "3":
+            titulo("Omicron Multi Tool | aeromicron | Soon.")
             clear()
-            print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter(second_shit2)))
+            print(Colorate.Vertical(Colors.green_to_red, Center.XCenter(second_shit2)))
             print(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("Gonna be updating ts soon")))
             input(Colorate.Diagonal(Colors.green_to_red, Center.XCenter("Press [ENTER] > ")))
 
